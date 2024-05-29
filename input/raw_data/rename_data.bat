abe7dea506833894a675eb3634bf23889e6dbfe6
shayantaqi <taqi.shayan@gmail.com>
2024-05-29 11:50:49 +0430

    First commit

---
+++ input/raw_data/rename_data.bat
@@ -0,0 +1,12 @@
+@echo off
+rename "EERA Public SchoolCBE - Data Entry Tool R3.xlsx" "Tool 0.EERA Public SchoolCBE - Data Entry Tool R3.xlsx"
+rename "EERA Public School - HeadmasterPrinciple Interview R3.xlsx" "Tool 1.EERA Public School - HeadmasterPrinciple Interview R3.xlsx"
+rename "EERA Public School - Light Tool R3.xlsx" "Tool 2.EERA Public School - Light Tool R3.xlsx"
+rename "EERA Public School - Student Document & Headcount R3.xlsx" "Tool 3.EERA Public School - Student Document & Headcount R3.xlsx"
+rename "EERA Public School - Teacher Tool R3.xlsx" "Tool 4.EERA Public School - Teacher Tool R3.xlsx"
+rename "EERA Public School - WASH Observation R3.xlsx" "Tool 5.EERA Public School - WASH Observation R3.xlsx"
+rename "EERA Public SchoolCBE - Parent Tool R3.xlsx" "Tool 6.EERA Public SchoolCBE - Parent Tool R3.xlsx"
+rename "EERA Public SchoolCBE - Shura Tool R3.xlsx" "Tool 7.EERA Public SchoolCBE - Shura Tool R3.xlsx"
+rename "EERA CBE - Class Level Tool R3.xlsx" "Tool 8.EERA CBE - Class Level Tool R3.xlsx"
+rename "EERA CBE - IP Level Tool R3.xlsx" "Tool 9.EERA CBE - IP Level Tool R3.xlsx"
+pause
---
+++ input/tools/rename_tool.bat
@@ -0,0 +1,12 @@
+@echo off
+rename "EERA Public School_CBE - Data Entry Tool - R3.xlsx" "Tool 0.EERA Public School_CBE - Data Entry Tool - R4.xlsx"
+rename "EERA Public School - Headmaster_Principle Interview - R3.xlsx" "Tool 1.EERA Public School - Headmaster_Principle Interview - R4.xlsx"
+rename "EERA Public School - Light Tool - R3.xlsx" "Tool 2.EERA Public School - Light Tool - R4.xlsx"
+rename "EERA Public School - Student Document & Headcount - R3.xlsx" "Tool 3.EERA Public School - Student Document & Headcount - R4.xlsx"
+rename "EERA Public School - Teacher Tool - R3.xlsx" "Tool 4.EERA Public School - Teacher Tool - R4.xlsx"
+rename "EERA Public School - WASH Observation - R3.xlsx" "Tool 5.EERA Public School - WASH Observation - R4.xlsx"
+rename "EERA Public School_CBE - Parent Tool - R3.xlsx" "Tool 6.EERA Public School_CBE - Parent Tool - R4.xlsx"
+rename "EERA Public School_CBE - Shura Tool - R3.xlsx" "Tool 7.EERA Public School_CBE - Shura Tool - R4.xlsx"
+rename "EERA CBE - Class Level Tool - R3.xlsx" "Tool 8.EERA CBE - Class Level Tool - R4.xlsx"
+rename "EERA CBE - IP Level Tool - R3.xlsx" "Tool 9.EERA CBE - IP Level Tool - R4.xlsx"
+pause
