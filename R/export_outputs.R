e017e29196aa02ecea5e10e7e81b65a02ce3ddd6
shayantaqi <taqi.shayan@gmail.com>
2024-11-30 22:20:40 +0330

    Adding exporting more outputs

--- R/export_outputs.R
+++ R/export_outputs.R
@@ -139,35 +139,35 @@ if(nrow(photo_status_log_discrep) > 0) {
   write.xlsx(photo_status_log_discrep, paste0(output_data_path, "issues/photo_status_log_issues/photo_status_log_issues_", Sys.Date(), ".xlsx"))
 }
 
-# if (nrow(repeat_sheet_issues) > 0){
-#   write.xlsx(repeat_sheet_issues, paste0(output_data_path, "issues/repeat_sheet_issues/repeat_sheet_issues_", Sys.Date(),".xlsx"))
-# }
+if (nrow(repeat_sheet_issues) > 0){
+  write.xlsx(repeat_sheet_issues, paste0(output_data_path, "issues/repeat_sheet_issues/repeat_sheet_issues_", Sys.Date(),".xlsx"))
+}
 
-# sm_question_issues_dfs = list(
-#   # "NA issues in select_multiple" = sm_na_issues,
-#   "parent_series_inconsistencies" = sm_check_result
-# )
-# 
-# if (nrow(sm_check_result) > 0){
-#   write.xlsx(sm_question_issues_dfs, paste0(output_data_path, "issues/sm_issues/sm_check_result_", Sys.Date(),".xlsx"))
-# }
+sm_question_issues_dfs = list(
+  # "NA issues in select_multiple" = sm_na_issues,
+  "parent_series_inconsistencies" = sm_check_result
+)
+
+if (nrow(sm_check_result) > 0){
+  write.xlsx(sm_question_issues_dfs, paste0(output_data_path, "issues/sm_issues/sm_check_result_", Sys.Date(),".xlsx"))
+}
 
 
-# if (nrow(wrong_choices_result) > 0){
-#   write.xlsx(wrong_choices_result, paste0(output_data_path, "issues/wrong_choices/wrong_choices_result_", Sys.Date(),".xlsx"))
-# }
+if (nrow(wrong_choices_result) > 0){
+  write.xlsx(wrong_choices_result, paste0(output_data_path, "issues/wrong_choices/wrong_choices_result_", Sys.Date(),".xlsx"))
+}
 
-# if (nrow(missing_translations) > 0){
-#   write.xlsx(missing_translations, paste0(output_data_path, "issues/translation_missing/missing_translations_", Sys.Date(),".xlsx"))
-# }
+if (nrow(missing_translations) > 0){
+  write.xlsx(missing_translations, paste0(output_data_path, "issues/translation_missing/missing_translations_", Sys.Date(),".xlsx"))
+}
 
-# if (nrow(Logic_check_result) > 0){
-#   write.xlsx(Logic_check_result, paste0(output_data_path, "issues/logic_checks/logic_checks_", Sys.Date(),".xlsx"))
-# }
-# 
-# if (nrow(logic_check_data_entry_final) > 0){
-#   write.xlsx(logic_chec`1`k_data_entry_final, paste0(output_data_path, "issues/logic_checks/logic_checks_Data_Entry_", Sys.Date(),".xlsx"))
-# }
+if (nrow(Logic_check_result) > 0){
+  write.xlsx(Logic_check_result, paste0(output_data_path, "issues/logic_checks/logic_checks_", Sys.Date(),".xlsx"))
+}
+
+if (nrow(logic_check_data_entry_final) > 0){
+  write.xlsx(logic_check_data_entry_final, paste0(output_data_path, "issues/logic_checks/logic_checks_Data_Entry_", Sys.Date(),".xlsx"))
+}
 
 # if (nrow(calculate_issues) > 0){
 #   write.xlsx(calculate_issues, paste0(output_data_path, "issues/calculate_issues/calculate_issues_", Sys.Date(),".xlsx"))
